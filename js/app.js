48e6f45ba9c09a6c96c654d6497598d36eb3f244
tektechnologies <info@tektechnologies.com>
2023-08-31 09:54:14 -0500

    validated html, add appjs, added css reset

--- js/app.js
+++ js/app.js
@@ -2,26 +2,24 @@
 console.log('app.js is connected');
 
 
-<script>
 
 function myFunction() {
-
+//we created a variable
   let text;
+//we asked the user for grace
+  let user = prompt('Do you seek the guidance of Grace?:', '');
+//if else conditional where we can check for truthy or falsy value
+  if (user === null || user === '') {
 
-  let user = prompt("Do you seek the guidance of Grace?:", "");
-
-  if (user == null || user == "") {
-
-    text = "User cancelled the prompt.";
+    text = 'User cancelled the prompt.';
 
   } else {
 
-    text = "Hello " + person + "! How are you?";
+    text = 'Hello ' + user + '! How are you?';
 
   }
 
-  document.getElementById("Prompt Example").innerHTML = text;
+  document.getElementById('promptExample').innerHTML = text;
 
 }
 
-</script>
