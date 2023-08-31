5aef535c9b0cc51380107929db10f8cd4d9a7166
tektechnologies <info@tektechnologies.com>
2023-08-31 09:30:25 -0500

    moved js to file, moved text to header, added style to the <p>

--- js/app.js
+++ js/app.js
@@ -1,2 +1,27 @@
 'use strict';
 console.log('app.js is connected');
+
+
+<script>
+
+function myFunction() {
+
+  let text;
+
+  let user = prompt("Do you seek the guidance of Grace?:", "");
+
+  if (user == null || user == "") {
+
+    text = "User cancelled the prompt.";
+
+  } else {
+
+    text = "Hello " + person + "! How are you?";
+
+  }
+
+  document.getElementById("Prompt Example").innerHTML = text;
+
+}
+
+</script>
