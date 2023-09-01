0a291a3187b765e04fece455fb7491a9b4fe8f1d
Tom <capps14e@gmail.com>
2023-09-01 13:11:03 -0400

    editied scripts

--- js/app.js
+++ js/app.js
@@ -4,12 +4,11 @@ console.log('app.js is connected');
 
 
 function myFunction() {
-//  console.log('proof that we are invoking our function');
- //we created a variable
+
   let text;
-//  we asked the user for grace
+
   let user = prompt('What is your name, Tarnished?:', '');
-// if else conditional where we can check for truthy or falsy value
+
   if (user === null || user === '') {
 
     text = 'User cancelled the prompt.';
